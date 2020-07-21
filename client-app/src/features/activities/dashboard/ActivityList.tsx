9953158a57641d217fb22df8fadada577fa207a7
Farhan Quadri <farchun@gmail.com>
2020-07-20 20:55:43 -0400

    End of section 6

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { SyntheticEvent } from 'react'
 import { Item, Button, Label, Segment } from 'semantic-ui-react'
 import { IActivity } from '../../../app/models/activity'
 
 interface IProps {
   activities: IActivity[]
   selectActivity: (id: string) => void;
-  deleteActivity: (id: string) => void;
+  deleteActivity: (e: SyntheticEvent<HTMLButtonElement>, id: string) => void;
+  submitting: boolean,
+  target: string
 }
 
-export const ActivityList: React.FC<IProps> = ({ activities, selectActivity, deleteActivity}) => {
+export const ActivityList: React.FC<IProps> = ({ activities, selectActivity, deleteActivity, submitting, target}) => {
   return (
     <Segment clearing>
       <Item.Group divided>
@@ -22,12 +24,16 @@ export const ActivityList: React.FC<IProps> = ({ activities, selectActivity, del
                 <div>{activity.city}, {activity.venue}</div>
               </Item.Description>
               <Item.Extra>
-                <Button onClick={() => selectActivity(activity.id)} 
+                <Button 
+                  onClick={() => selectActivity(activity.id)} 
                   floated='right'
                   content='View'
                   color='blue'
                 />
-                <Button onClick={() => deleteActivity(activity.id)} 
+                <Button 
+                  name={activity.id}
+                  loading={target === activity.id && submitting}
+                  onClick={(e) => deleteActivity(e, activity.id)} 
                   floated='right'
                   content='Delete'
                   color='red'
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -8,9 +8,10 @@ interface IProps {
   activity: IActivity;
   createActivity: (activity: IActivity) => void;
   editActivity: (activity: IActivity) => void;
+  submitting: boolean;
 }
 
-export const ActivityForm: React.FC<IProps> = ({ setEditMode, activity: initialFormState, createActivity, editActivity }) => {
+export const ActivityForm: React.FC<IProps> = ({ setEditMode, activity: initialFormState, createActivity, editActivity, submitting }) => {
   const initializeForm = () => {
     if (initialFormState) {
       return initialFormState;
@@ -55,7 +56,7 @@ export const ActivityForm: React.FC<IProps> = ({ setEditMode, activity: initialF
         <Form.Input onChange={handleInputChange} name="date" type="datetime-local" placeholder="Date" value={activity.date} />
         <Form.Input onChange={handleInputChange} name="city" placeholder="City" value={activity.city} />
         <Form.Input onChange={handleInputChange} name="venue" placeholder="Venue" value={activity.venue} />
-        <Button floated="right" positive type="submit" content="Submit" />
+        <Button loading={submitting} floated="right" positive type="submit" content="Submit" />
         <Button onClick={() => setEditMode(false)} floated="right" type="button" content="Cancel" />
       </Form>
     </Segment>
